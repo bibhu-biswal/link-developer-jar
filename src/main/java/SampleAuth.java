a617d94fe543cb9388b9f15fd33dc482d431e7ee
Aditi Mehta <akmehta@ucsd.edu>
2014-07-07 16:12:13 -0700

    Added script for testing and dependencies [#74333188]

--- src/main/java/SampleAuth.java
+++ src/main/java/SampleAuth.java
@@ -1,11 +1,14 @@
 import java.util.HashMap;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.core.UriBuilder;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+
 import org.apache.commons.codec.binary.Base64;
 
 /*
@@ -14,27 +17,32 @@ import org.apache.commons.codec.binary.Base64;
 public class SampleAuth {
 
 	final String LP_API_HOST = "https://www.livepaperapi.com";
-	String token = null;  
+	public String token = null;  
+	public int responseCode = 0;
 	
 	/*
 	 * Authorize the client. 
-	 * @param map A HashMap with the key-value pairs ("id", "client-id") and ("secret", "client-secret")
-	 * Decide what param to keep: HashMap might be annoying to create and type while using jar
+	 * @param clientID The clientID provided in the access credentials
+	 * @param secret The client secret provided in the access credentials
+	 * 
 	 */
-	public void auth(String clientID, String secret)  //change param to HashMap<String, String> map if testing with yml file
-	{
+	public void auth(HashMap<String, String> map)  //change param to HashMap<String, String> map if testing with yml file
+	{											   // else String clientID, String secret
 		//While refactoring: Convert to static method that returns an authorized object of inner class (possibly)
 		
-		/*
-		 * UNCOMMENT IF TESTING WITH YML FILE
-		 * String id = map.get("id");
-		 * String secret = map.get("secret");
-		 * authorize(id, secret);
-		 * 
-		 */
 		
-		//comment out if testing wiht yml file
-		authorize(clientID, secret);
+		 //COMMENT IF NOT TESTING WITH YML FILE
+		 String id = map.get("id");
+		 String secret = map.get("secret");
+		 authorize(id, secret);
+		  
+		//uncomment if not testing with yml file
+		 /*
+		  * if (clientID == null || secret == null)
+		  * 	throw new NullPointerException("Null arguments not accepted.");
+		  */
+		//authorize(clientID, secret);
+		 
 		
 	}
 	
@@ -54,17 +62,12 @@ public class SampleAuth {
 		WebResource webResource = client.resource(UriBuilder.fromUri(LP_API_HOST+"/auth/token").build());
 
 		ClientResponse response = webResource.header("Content-Type", "application/x-www-form-urlencoded").accept("application/json").header("Authorization", toBeSent).post(ClientResponse.class, body);
-		int responseCode = response.getStatus();
-		
-		System.out.println("Response code: "+ responseCode);
+		responseCode = response.getStatus();
 		
 		//Exceptions and other response codes handling: In the JAR, ideally there should be some sort of exception, or null
 		//obejct returned
 		if(responseCode== 200)
 			token = tokenizer(response.getEntity(String.class)); 
-		else
-			System.out.println("Invalid!");
-		System.out.println("Token: "+ token);
 	}
 
 	/*
--- src/test/java/TestAuth.java
+++ src/test/java/TestAuth.java
@@ -1,8 +1,15 @@
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 
 
@@ -14,23 +21,56 @@ import org.yaml.snakeyaml.Yaml;
  *  >secret: your_client_secret
  *  
  *  */
-public class TestAuth {
+public class TestAuth extends TestCase {
+	
+	SampleAuth test;
+	Yaml obj;
+	InputStream input;
+	HashMap<String,String> map;
+	
+	@SuppressWarnings("unchecked")
+	public void setUp()
+	{
+		test = new SampleAuth();
+		obj = new Yaml();
+		System.getProperties().put("http.proxySet", "true");
+		try {
+			input = new FileInputStream(new File("src/main/java/myauth.yml"));
+		} catch (FileNotFoundException e) {
+			System.out.println("File myauth.yml not found. Must have a myauth.yml file in src/main/java/client");
+		}
+		map = (HashMap<String, String>) obj.load(input);
+	}
+	
+	@Test
+	public void testCorrectKey()
+	{
+		test.auth(map);
+		assertEquals("Response code must be 200 (OK).","200", ""+test.responseCode);
+		assertNotNull(test.token, "Token cannot be null.");
+	}
+	
+	@Test
+	public void testIncorrectID()
+	{
+		map.put("id", "wrong");
+		test.auth(map);
+		assertEquals("Response code must be 401 (Invalid ID).","401", ""+test.responseCode);
+		assertEquals("Token should be null.", test.token, null);
+	}
+	
+	@Test
+	public void testIncorrectSecret()
+	{
+		map.put("secret", "wrong");
+		test.auth(map);
+		assertEquals("Response code must be 401 (Invalid secret).","401", ""+test.responseCode);
+		assertEquals("Token should be null.", test.token, null);
+	}
 
+	
 	public static void main(String[] args) throws IOException
 	{
-		SampleAuth test = new SampleAuth();
-		
-		Yaml obj = new Yaml();
-		
-	    InputStream input = new FileInputStream(new File("src/main/java/myauth.yml"));
-		
-	    //Does the input have to be a map (for the final jar)? Might be easier as an array of strings
-	    
-	    //Uncomment if testing with yml file
-	    //HashMap<String,String> map = (HashMap<String, String>) obj.load(input);
-		//test.auth(map);
-	    
-	    //comment out if testing with yml file
-	    test.auth("clientID", "secret");
+		  junit.textui.TestRunner.run(TestAuth.class);
 	}
 }
