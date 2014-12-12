9b62d7cfc4f5c09ac0d3a462599253aa4a36f7bf
Glenn Sisson <glenn@hp.com>
2014-12-12 12:05:58 -0800

    v0.0.3: no more external classes embedded in the LivePaper.jar [#84343016]

--- compile_and_test_jar.sh
+++ compile_and_test_jar.sh
@@ -1,18 +1,52 @@
 #!/bin/sh
 
-rm -f dep/com/hp/*
-echo "------------------------------------"
-echo "Compiling LivePaper.java..."
-javac -cp "./dep/" lib/LivePaper.java || exit 1
+RED="\033[1;31m"
+CYAN="\033[1;36m"
+RESET="\033[0m"
 
-echo "------------------------------------"
-echo "Creating JAR..."
-mv lib/*.class dep/com/hp
-cd dep
-jar -cf livepaper.jar * || exit 1
-mv livepaper.jar ..
-cd ..
+usejar() {
+  bin=""
+  jar="${1%.jar}"
+  url="$2"
+  case $jar in
+    *-bin.tar.gz)
+      bin=$jar
+      jar=${jar%-bin.tar.gz}
+      ;;
+  esac
+  mkdir -p jars
+  if [ -f jars/$jar.jar -a ! -s jars/$jar.jar ]; then
+    rm jars/$jar.jar # previous download problems can leave an empty file... remove it
+  fi
+  if [ ! -f jars/$jar.jar ]; then
+    echo "${CYAN}downloading $jar.jar...${RESET}"
+    if [ "$bin" != "" ]; then
+     echo + curl -s $url/${bin} \| gunzip \| tar -xvf - --include '*'$jar.jar
+             curl -s $url/${bin}  | gunzip  | tar -xvf - --include '*'$jar.jar
+      mv $jar/$jar.jar jars/ && rmdir $jar
+    else
+     echo + curl -s $url/$jar.jar \> jars/$jar.jar
+             curl -s $url/$jar.jar  > jars/$jar.jar
+    fi
+  fi
+  tar -tvf jars/$jar.jar | grep MANIFEST > /dev/null
+  if [ $? = 1 ];then
+  	echo "${RED}ERROR: unable to obtain jar \"$jar\"!${RESET}"
+  	exit 1
+  fi
+  CLASSPATH="$CLASSPATH:jars/${jar}.jar"
+}
 
-echo "------------------------------------"
-echo "Running tests..."
+export CLASSPATH="$HOME/src/lpp_client_java/livepaper.jar"
+usejar "boon-0.24"             "http://central.maven.org/maven2/io/fastjson/boon/0.24"
+usejar "jersey-client-1.18.3"  "http://central.maven.org/maven2/com/sun/jersey/jersey-client/1.18.3"
+usejar "jersey-core-1.18.3"    "http://central.maven.org/maven2/com/sun/jersey/jersey-core/1.18.3"
+
+echo "${CYAN}Compiling LivePaper.java...${RESET}"
+javac com/hp/LivePaper.java || exit 1
+
+echo "${CYAN}Creating LivePaper.jar...${RESET}"
+jar -cf livepaper.jar com || exit 1
+
+echo "${CYAN}Testing the jar...${RESET}"
 ./test_jar.sh
--- test_jar.sh
+++ test_jar.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-keyfile="../playground/lpapi_exemplar_java/mykeys.txt"
-script="../playground/lpapi_exemplar_java/TestLinkAPI.sh"
-rm -f url qr_code.png watermark.jpg
-if [ ! -f "$script" ];then
-  echo "ERROR: test script not found: $script"
-  exit 1
+cd "../playground/lpapi_exemplar_java/"
+if [ $? != 0 ]; then
+  echo "ERROR: cannot find directory holding test script!"; exit 1
 fi
-$script -k $keyfile -s http://en.wikipedia.org/wiki/URL_shortening | tee url || exit 1
-$script -k $keyfile -q http://en.wikipedia.org/wiki/QR_code -o qr_code.png || exit 1
-$script -k $keyfile -w http://en.wikipedia.org/wiki/Watermark -o watermark.jpg \
-        -i http://upload.wikimedia.org/wikipedia/commons/8/82/Watermarks_20_Euro.jpg || exit 1
-open $(tail -1 url)
+
+./TestLinkAPI.sh
